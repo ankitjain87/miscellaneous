2fd039fa274dcc7ff71d9f92085a4aa992a58282
ankitjain87 <ankit.it09@gmail.com>
2016-06-06 16:55:16 +0530

    Create basecase_defender.js

---
+++ basecase_defender.js
@@ -0,0 +1,33 @@
+//http://jsfiddle.net/j0eboygz/1/
+
+function paddleMoves(nearestRock) {
+    var y = - parseInt(Math.tan(nearestRock.radians) * nearestRock.distance)
+    var moves = [];
+    var pos = y < 0 ? -1 : 1;
+    y = Math.abs(y);
+
+    for (i=0; i < y; i++) {
+        moves.push(pos);
+    }
+
+    for (i=0; i < (nearestRock.distance - y); i++) {
+        moves.push(0);
+    }
+
+  return moves
+}
+
+defender.start(
+  function notify_player(rocks, paddle_y){ 
+      var moves = [];
+      var nearestRock = 0;
+
+      for (var i = 1; i < rocks.length; i++) {
+          if (rocks[i].distance < rocks[nearestRock].distance) {
+              nearestRock = i;
+          }
+      }
+
+      return paddleMoves(rocks[nearestRock]);
+  }
+);
